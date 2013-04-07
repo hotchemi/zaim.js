b488f3332736f3232e32ec677c339c3ec327400e
hotchemi <hot.chemistry21@gmail.com>
2013-04-08 00:44:48 +0900

    0.0.8 release.

--- README.md
+++ README.md
@@ -1,4 +1,157 @@
-zaim.js
+Zaim.js [![Build Status](https://secure.travis-ci.org/hotchemi/zaim.js.png)](http://travis-ci.org/hotchemi/zaim.js)
 =======
 
 Node.js library for the Zaim API.
+
+##Install
+Install from npm:
+
+    $ npm install zaim
+
+##Usage
+Please see [API doc](https://dev.zaim.net/home/api) for further details.
+
+###Constructor
+```javascript
+var Zaim = require('zaim');
+
+var zaim = new Zaim({
+  // must configure consumer key and secret
+  consumerKey: 'CONSUMER KEY',
+  consumerSecret: 'CONSUMER SECRET',
+  // option params
+  accessToken: 'ACCESS TOKEN',
+  accessTokenSecret: 'ACCESS TOKEN SECRET',
+  callback: 'CALLBACK URL'
+});
+```
+###Authorization url
+```javascript
+// https://www.zaim.net/users/auth?oauth_token=***
+console.log(zaim.getAuthorizationUrl());
+```
+###Get access token and Secret
+```javascript
+zaim.getOAuthAccessToken(pin, function(err, token, secret, results) {
+  console.log(token); //access token
+  console.log(secret); //access token secret
+});
+```
+###Set access token and secret
+```javascript
+zaim.setAccessToken('accessToken');
+zaim.setAccessTokenSecret('accessTokenSecret');
+```
+###User
+```javascript
+zaim.getCredentials(function(data, err) {
+  console.log(data);
+});
+```
+###Create payment
+```javascript
+zaim.createPay({
+  category_id: 'category_id', //required
+  genre_id: 'genre_id', //required
+  price: 100, //required
+  date: '2013-04-10',
+  comment: 'comment: memo (within 100 characters)',
+  active: 'public or private（0:private 1:public, default is 0）'
+}, function(data, err){
+  console.log(data);
+});
+```
+###Create income
+```javascript
+zaim.createIncome({
+  category_id: 'category_id', //required
+  price: 100, //required
+  date: '2013-04-10',
+  comment: 'comment: memo (within 100 characters)',
+  active: 'public or private（0:private 1:public, default is 0）'
+}, function(data, err){
+  console.log(data);
+});
+```
+###Get money
+```javascript
+zaim.getMoney({
+  category_id: 'narrow down by category_id',
+  genre_id: 'narrow down by genre_id',
+  type: 'narrow down by type (pay or income)',
+  order: 'sort by id or date (default : date)',
+  start_date: 'the first date (Y-m-d format)',
+  end_date: 'the last date (Y-m-d format)',
+  page: 'number of current page (default 1)',
+  limit: 'number of items per page (default 20, max 100)'
+}, function(data, err) {
+  console.log(data);
+});
+```
+or
+```javascript
+zaim.getMoney(function(data, err) {
+  console.log(data);
+});
+```
+###Get payment categories
+```javascript
+zaim.getPayCategories({
+  lang: 'If you set this parameter with "ja", response title becomes Japanese.'
+}, function(data, err) {
+  console.log(data);
+});
+```
+or
+```javascript
+zaim.getPayCategories(function(data, err) {
+  console.log(data);
+});
+```
+###Get income categories
+```javascript
+zaim.getIncomeCategories({
+  lang: 'If you set this parameter with "ja", response title becomes Japanese.'
+}, function(data, err) {
+  console.log(data);
+});
+```
+or
+```javascript
+zaim.getIncomeCategories(function(data, err) {
+  console.log(data);
+});
+```
+###Get payment genres
+```javascript
+zaim.getPayGenres({
+  lang: 'If you set this parameter with "ja", response title becomes Japanese.'
+}, function(data, err) {
+  console.log(data);
+});
+```
+or
+```javascript
+zaim.getPayGenres(function(data, err) {
+  console.log(data);
+});
+```
+###Get currencies
+```javascript
+zaim.getCurrencies(function(data, err) {
+  console.log(data);
+});
+```
+##Test
+
+    $ npm test
+
+## Release note
+* 2013/04/09 0.0.1 release.
+* 2013/04/30 0.0.7 release.
+* 2013/04/30 0.0.8 release.
+
+##Link
+* https://dev.zaim.net/home/api
+* https://npmjs.org/package/zaim
+* http://hotchemi.github.io/zaim.js
---
+++ test/zaim.js
@@ -0,0 +1,210 @@
+var expect = require('expect.js'),
+  Zaim = require('../lib/zaim');
+
+describe('Constructor suite', function() {
+  it('should throw a error without consumer key and secret', function() {
+    expect(function() {
+      new Zaim({});
+    }).to.throwError(function(e) {
+        expect(e.message).to.equal('ConsumerKey and secret must be configured.');
+      });
+  });
+
+  it('should throw a error without consumer secret', function() {
+    expect(function() {
+      new Zaim({
+        consumerKey: "consumerKey"
+      });
+    }).to.throwError();
+  });
+
+  it('should throw a error without consumer key', function() {
+    expect(function() {
+      new Zaim({
+        consumerSecret: "consumerSecret"
+      });
+    }).to.throwError();
+  });
+
+  it('should not throw a error with valid params', function() {
+    expect(function() {
+      new Zaim({
+        consumerKey: "consumerKey",
+        consumerSecret: "consumerSecret"
+      });
+    }).to.not.throwError();
+  });
+});
+
+describe('getAuthorizationUrl() suite', function() {
+  it('should throw error without callback', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    expect(function() {
+      zaim.getAuthorizationUrl();
+    }).to.throwError(function(e) {
+        expect(e.message).to.equal('ConsumerKey, secret and callback url must be configured.');
+      });
+  });
+
+  it('should not throw error with valid parameters', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret',
+      callback: 'http://zaim.net'
+    });
+    expect(function() {
+      zaim.getAuthorizationUrl();
+    }).to.not.throwError();
+  });
+});
+
+describe('setter suite', function() {
+  it('should equal accessToken', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    zaim.setAccessToken('accessToken')
+    expect(zaim.token).to.equal('accessToken');
+  });
+
+  it('should equal accessTokenSecret', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    zaim.setAccessTokenSecret('accessTokenSecret')
+    expect(zaim.secret).to.equal('accessTokenSecret');
+  });
+});
+
+describe('createPay suite', function() {
+  it('should throw error with empty params', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    expect(function() {
+      zaim.createPay({}, function(data, err){});
+    }).to.throwError(function(e) {
+        expect(e.message).to.equal('Invalid parameters.category_id, genre_id and price are necessary.');
+      });
+  });
+
+  it('with valid parameters', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    // throw error because set invalid consumer key and secret.
+    expect(function() {
+      zaim.createPay({
+        category_id: 'category_id',
+        genre_id: 'genre_id',
+        price: 100,
+        date: '2013-04-10' },
+        function(data, error) {});
+    }).to.throwError(function(e) {
+        expect(e.message).to.not.equal('Invalid parameters.category_id, genre_id and price are necessary.');
+      });
+  });
+});
+
+describe('createIncome suite', function() {
+  it('should throw error with empty params', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    expect(function() {
+      zaim.createIncome({}, function(data, err){});
+    }).to.throwError(function(e) {
+        expect(e.message).to.equal('Invalid parameters.category_id and price are necessary.');
+      });
+  });
+
+  it('with valid parameters', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    expect(function() {
+      zaim.createIncome({
+          category_id: 'category_id',
+          price: 100,
+          date: '2013-04-10' },
+        function(data, error){});
+    }).to.throwError(function(e) {
+        expect(e.message).to.not.equal('Invalid parameters.category_id and price are necessary.');
+      });
+  });
+});
+
+describe('private method suite', function() {
+  it('should throw error without access token and secret', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret'
+    });
+    expect(function() {
+      zaim._httpGet("http://zaim.net", function(data, err){});
+    }).to.throwError(function(e) {
+        expect(e.message).to.equal('accessToken and tokenSecret must be configured.');
+      });
+  });
+
+  it('should throw error without access token secret', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret',
+      accessToken: 'accessToken'
+    });
+    expect(function() {
+      zaim._httpGet("http://zaim.net/", function(data, err){});
+    }).to.throwError(function(e) {
+        expect(e.message).to.equal('accessToken and tokenSecret must be configured.');
+      });
+  });
+
+  it('should throw error without access token', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret',
+      accessTokenSecret: 'accessTokenSecret'
+    });
+    expect(function() {
+      zaim._httpPost("http://zaim.net/", {}, function(data, err){});
+    }).to.throwError(function(e) {
+        expect(e.message).to.equal('accessToken and tokenSecret must be configured.');
+      });
+  });
+
+  it('should not throw error with access token and secret', function() {
+    var zaim = new Zaim({
+      consumerKey:'consumerKey',
+      consumerSecret: 'consumerSecret',
+      accessToken: 'accessToken',
+      accessTokenSecret: 'accessTokenSecret'
+    });
+    expect(function() {
+      zaim._httpPost("http://zaim.net/", {}, function(data, err){});
+    }).to.not.throwError();
+  });
+
+  it('should return valid date today', function() {
+    var zaim = new Zaim({
+        consumerKey:'consumerKey',
+        consumerSecret: 'consumerSecret',
+        accessToken: 'accessToken',
+        accessTokenSecret: 'accessTokenSecret'
+      }),
+      ymd = new Date(),
+      yy = ymd.getYear() % 1900 + 1900,
+      mm = ymd.getMonth() + 1,
+      dd = ymd.getDate();
+    expect(zaim._getCurrentDate()).to.equal(yy + '-' + mm + '-' + dd);
+  });
+});
